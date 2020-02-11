7554bb475d23f8d2d1649926389f023759468645
dogcolley <jinji0326@naver.com>
2020-02-11 17:16:27 +0900

    no message

--- html/index.php
+++ html/index.php
@@ -3,12 +3,13 @@
     include_once("./_head.php");
 ?>
 <?if($is_member){?>
-<div id="contributions_info" v-if="showContributions.length !== 0" class="U_area01 T_pd_top100 T_ps_rl clear">
+<div id="contributions_info" v-if="showContributions.length !== 0" class="U_area01 T_pd_top100 T_ps_rl clear PC_wd_sz640">
     <div class="userInfo T_ft_rem12 U_ft_c333 PC_pd_wd40">
         <div>검색대상: <span>{{setName}}</span></div>
         <div>오늘 : <span>{{today}}</span> </div>
         <div>사용년도 데이터 : <span>{{setYear}} </span></div>
-        <div>오늘 커밋여부 : <span>{{todayComit ? "인증함" : "인증안함"}}</span></div>
+        <div>오늘 커밋여부 : <span>{{todayComit ? "인증함" : "인증안함"}}<button v-if="todayComit">인증하기</button></span></div>
+        
     </div>
     <ul class="commitYear T_ly_3   T_ft_ct ">
         <li v-for="(data, index) in years" :key="index"><button type="button" v-bind:class="{active: years[index].year == setYear ? true : false}" v-on:click="chageData(years[index].year)">{{years[index].year}} ({{years[index].total}})</button></li>
