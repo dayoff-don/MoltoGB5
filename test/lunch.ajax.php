1113a0d49e0566bbfb630131f9a1712d3b46c3e4
dogcolley <jinji0326@naver.com>
2020-05-01 20:43:33 +0900

    뭐먹지 아젝스 통신만함녀 끝

--- test/lunch.ajax.php
+++ test/lunch.ajax.php
@@ -1,4 +1,12 @@
 <?php
 include_once('../common.php');
 
+$arr['ip'] = $_SERVER["REMOTE_ADDR"];
+$arr['wr_id'] = $_GET['wr_id']; 
+$json = my_json_encode($arr);
+
+//여기에 날짜랑 wr_id맞춰서 카운터 다시해서 뿌려주기
+
+echo $json;
+
 ?>
--- test/lunch.php
+++ test/lunch.php
@@ -1,8 +1,23 @@
 <?include_once('./_common.php');?>
+<?
+
+$bo_table = "lunch";
+$g5['lunch_table'] = 'add_lunch';
+$set_sql_table = $g5['write_prefix'].$bo_table;
+$sql = "select * from ".$set_sql_table;
+$sql_query = sql_query($sql);
+
+$list = array();
+while($row = sql_fetch_array($sql_query)){
+    $list[] = $row;
+}
+
+?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
 
 <style>
 body,html{font-size:1em;margin:0;padding:0}
-
 @media screen and (max-width : 640px){
     body,html{font-size:0.7em}
 }
@@ -21,6 +36,26 @@ body,html{font-size:1em;margin:0;padding:0}
 .U_td07{width:10%;max-width:120px}
 </style>
 
+<script>
+    $(function(){
+        $('.choice').on('click',function(){
+            var id = $(this).data('id');
+            $.ajax({ url: "http://grassgardener.kr/test/lunch.ajax.php", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소 
+            data: { wr_id : id,
+                    action : 'choice'
+            }, // HTTP 요청과 함께 서버로 보낼 데이터 
+            method: "GET", // HTTP 요청 메소드(GET, POST 등) 
+            dataType: "json" // 서버에서 보내줄 데이터의 타입 }) // HTTP 요청이 성공하면 요청한 데이터가 done() 메소드로 전달됨. 
+            }).done(function(json) {console.log(json) }) 
+            // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨. 
+            .fail(function(xhr, status, errorThrown) {console.log('에러') }) 
+            //.append("상태: " + status); 
+            // .always(function(xhr, status) { $("#text").html("요청이 완료되었습니다!"); });
+
+        });
+    });
+
+</script>
 
 <div class="U_wrap">
     <div class="U_cell">
@@ -33,20 +68,22 @@ body,html{font-size:1em;margin:0;padding:0}
                     <th>가게이름</th>
                     <th>머지가능</th>
                     <th>배달가능</th>
-                    <th>투표</th>
+                    <th>금일투표</th>
                     <th>누적투표</th>
                 </tr>
             </head>
             <tbody>
+                <?for($i=0; $i < count($list); $i++){?>
                 <tr>
-                    <td class="U_td01">한식</td>
-                    <td class="U_td02" title="위치위치"><a href="">확인</a></td>
-                    <td class="U_td03">돈까스이야기</td>
-                    <td class="U_td04">o</td>
-                    <td class="U_td05">o</td>
-                    <td class="U_td06">5 <button>투표하기</button></td>
-                    <td class="U_td07">999</td>
+                    <td class="U_td01"><?=$list[$i]['ca_name']?></td>
+                    <td class="U_td02" title="위치위치" target="_blank"><a href="<?=$list[$i]['wr_link1']?>"><?=$list[$i]['wr_1']?></a></td>
+                    <td class="U_td03"><?=$list[$i]['wr_subject']?></td>
+                    <td class="U_td04"><?=$list[$i]['wr_2'] ? 'o':'x' ?></td>
+                    <td class="U_td05"><?=$list[$i]['wr_3'] ? 'o':'x'?></td>
+                    <td class="U_td06"><span>0</span> <button class="choice" data-id="<?=$list[$i]['wr_id']?>">투표하기</button></td>
+                    <td class="U_td07"><span>0</span></td>
                 </tr>
+                <?}?>
             </tbody>
         </table>
     </div>
