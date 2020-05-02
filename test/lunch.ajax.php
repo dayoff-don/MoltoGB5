9e8fc69c09a8d2d185969de130ae6d60fcd31f11
dogcolley <jinji0326@naver.com>
2020-05-02 11:22:07 +0900

    오늘 뭐먹을까 추가 완료

--- test/lunch.ajax.php
+++ test/lunch.ajax.php
@@ -1,12 +1,53 @@
 <?php
 include_once('../common.php');
 
-$arr['ip'] = $_SERVER["REMOTE_ADDR"];
-$arr['wr_id'] = $_GET['wr_id']; 
+//common
+$bo_table = "lunch";
+$g5['lunch_table'] = 'add_lunch';
+$today = date('Y-m-d');
+$ip = $_SERVER["REMOTE_ADDR"];
+$wr_id = $_GET['wr_id']; 
+//check
+$arr['ip'] = $ip;
+$arr['wr_id'] =  $wr_id;
+$arr['today'] = $today;
+
+//action mode choice
+if($_GET['action'] == "choice"){
+    $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$arr['wr_id']."' AND ip ='".$ip."' AND reg_date = '".$today."'";
+    //$arr['sql'] = $sql;
+    $sql_query = sql_fetch($sql);
+
+    if($sql_query['cnt'] > 0){
+        //이미 있음
+        $arr['result'] = "already";
+        $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$arr['wr_id']."' AND reg_date = '".$today."'";
+        $sql_query = sql_fetch($sql);
+        $arr['cnt'] = $sql_query['cnt'];
+    }else{
+        //없으니 추가
+        $sql = " INSERT INTO ".$g5['lunch_table']." 
+            SET wr_id = '$wr_id',
+                ip = '$ip',
+                reg_date = '$today' ";
+
+        $sql_query = sql_fetch($sql);
+        //$arr['sql2'] = $sql;
+        $arr['result'] = "add";
+
+        $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$arr['wr_id']."' AND reg_date = '".$today."'";
+        $sql_query = sql_fetch($sql);
+        $arr['cnt'] = $sql_query['cnt'];
+
+        $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$arr['wr_id']."' AND reg_date = '".$today."'";
+        $sql_query = sql_fetch($sql);
+        $arr['cnt_total'] = $sql_query['cnt'];
+        
+    }
+}
+
+//make json 
 $json = my_json_encode($arr);
-
-//여기에 날짜랑 wr_id맞춰서 카운터 다시해서 뿌려주기
-
 echo $json;
 
 ?>
--- test/lunch.php
+++ test/lunch.php
@@ -1,8 +1,14 @@
 <?include_once('./_common.php');?>
 <?
 
+//common
 $bo_table = "lunch";
 $g5['lunch_table'] = 'add_lunch';
+$today = date('Y-m-d');
+$ip = $_SERVER["REMOTE_ADDR"];
+$wr_id = $_GET['wr_id']; 
+
+//set_write_table sql 
 $set_sql_table = $g5['write_prefix'].$bo_table;
 $sql = "select * from ".$set_sql_table;
 $sql_query = sql_query($sql);
@@ -12,6 +18,32 @@ while($row = sql_fetch_array($sql_query)){
     $list[] = $row;
 }
 
+function check_today($wr_id){
+    global $g5;
+    global $today;
+    $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$wr_id."' AND reg_date = '".$today."'";
+    $sql_query = sql_fetch($sql);
+    return $sql_query['cnt'];
+}
+
+function check_my($wr_id){
+    global $g5;
+    global $today;
+    global $ip;
+    $sql = $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$wr_id."' AND ip ='".$ip."' AND reg_date = '".$today."'";
+    $sql_query = sql_fetch($sql);
+    return $sql_query['cnt'];
+}
+
+function check_total($wr_id){
+    global $g5;
+    global $today;
+    $sql = "select COUNT(*) as cnt from ".$g5['lunch_table']." where wr_id = '".$wr_id."' ";
+    $sql_query = sql_fetch($sql);
+    return $sql_query['cnt'];
+}
+ 
+echo $_SERVER["REMOTE_ADDR"];
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
@@ -39,6 +71,7 @@ body,html{font-size:1em;margin:0;padding:0}
 <script>
     $(function(){
         $('.choice').on('click',function(){
+            var this_tag = $(this);
             var id = $(this).data('id');
             $.ajax({ url: "http://grassgardener.kr/test/lunch.ajax.php", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소 
             data: { wr_id : id,
@@ -46,7 +79,16 @@ body,html{font-size:1em;margin:0;padding:0}
             }, // HTTP 요청과 함께 서버로 보낼 데이터 
             method: "GET", // HTTP 요청 메소드(GET, POST 등) 
             dataType: "json" // 서버에서 보내줄 데이터의 타입 }) // HTTP 요청이 성공하면 요청한 데이터가 done() 메소드로 전달됨. 
-            }).done(function(json) {console.log(json) }) 
+            }).done(function(json) {console.log(json) 
+                if(json.result == "add"){
+                    alert('추가가 되었습니다.')
+                    this_tag.siblings('span').text(json.cnt);
+                    this_tag.parent().siblings('.U_td07').find('span').text(json.cnt_total);
+                    this_tag.remove();
+                }else if(json.result == "already"){
+                    alert('이미추가했습니다.')
+                }
+            }) 
             // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨. 
             .fail(function(xhr, status, errorThrown) {console.log('에러') }) 
             //.append("상태: " + status); 
@@ -73,15 +115,19 @@ body,html{font-size:1em;margin:0;padding:0}
                 </tr>
             </head>
             <tbody>
-                <?for($i=0; $i < count($list); $i++){?>
+                <?for($i=0; $i < count($list); $i++){
+                    $today_num = check_today($list[$i]['wr_id']);
+                    $total_num = check_total($list[$i]['wr_id']);
+                    $my_checked = check_my($list[$i]['wr_id'])
+                ?>
                 <tr>
                     <td class="U_td01"><?=$list[$i]['ca_name']?></td>
                     <td class="U_td02" title="위치위치" target="_blank"><a href="<?=$list[$i]['wr_link1']?>"><?=$list[$i]['wr_1']?></a></td>
                     <td class="U_td03"><?=$list[$i]['wr_subject']?></td>
                     <td class="U_td04"><?=$list[$i]['wr_2'] ? 'o':'x' ?></td>
                     <td class="U_td05"><?=$list[$i]['wr_3'] ? 'o':'x'?></td>
-                    <td class="U_td06"><span>0</span> <button class="choice" data-id="<?=$list[$i]['wr_id']?>">투표하기</button></td>
-                    <td class="U_td07"><span>0</span></td>
+                    <td class="U_td06"><span><?=$today_num?></span> <?= $my_checked > 0 ? '' : '<button class="choice" data-id="'.$list[$i]['wr_id'].'">투표하기</button></td>'?>
+                    <td class="U_td07"><span><?=$total_num?></span></td>
                 </tr>
                 <?}?>
             </tbody>
