b068495efcc80903a3abde90b31cc16c44e7b3fb
dogcolley <jinji0326@naver.com>
2020-05-04 14:26:10 +0900

    d

--- api/g5_member_total_api.php
+++
@@ -1,5 +0,0 @@
-<?
-    include_once("./_common.php");
-    $row = sql_fetch(" select count(*) as cnt from $g5[member_table] ");
-    echo $row['cnt'];
-?>
--- api/gitGetName_api.php
+++
@@ -1,9 +0,0 @@
-<?
-include_once("./_common.php"); 
-if($is_member){
-    echo $member['mb_1'];
-}else{
-    die;
-}
-
-?>
