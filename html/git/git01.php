78a2c257548de7083443d866be2d65f941ad7b6e
dogcolley <jinji0326@naver.com>
2020-05-08 22:58:38 +0900

    추가 스타일

--- html/git/git01.php
+++ html/git/git01.php
@@ -21,11 +21,11 @@
                 </ul>
             </div>
 
-            <div class="T_mg_btm30 U_bg_cfff U_ov_at T_ht_sz50 U_ft_c000" style="border-radius:5px">
+            <div class="T_mg_btm30 U_bg_cfff U_ov_at T_ht_sz50 U_ft_c000 U_info01" style="border-radius:5px">
                <ul class="T_ht_full">
                     <li v-for="(data, index) in ranker" :key="index" class="T_pd_ht15 T_lh_px20 T_ht_full T_pd_Pwd10 clear">
                         <span>{{index+1}}</span>.
-                        <span>{{data.git_id}}</span>
+                        <span>{{data.git_id}}</span>  
                         <span class="T_fl_rt">{{data.git_cnt}}</span>
                     </li>
                     <li v-if="noData" class="T_pd_ht15 T_lh_px20 T_ht_full T_pd_Pwd10 clear">자료가 없습니다.</li>
