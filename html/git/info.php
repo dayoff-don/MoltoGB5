cb09012b47c595649a1bacfbc20585f156db8540
dogcolley <jinji0326@naver.com>
2020-05-24 18:01:08 +0900

    인증이미지 및 링크수정

--- html/git/info.php
+++ html/git/info.php
@@ -16,10 +16,14 @@
     }
     if($ch_data){
         $og_description = $set_git_id.'님 깃허브 금일 인증하셨습니다.' ;
+        $og_url = G5_IMG_URL.'/ok.png';
     }
-    if(!$og_description) $og_description = '인증정보가 없습니다. ㅠㅠ';
-    
+    if(!$og_description) {
+        $og_description = '인증정보가 없습니다. ㅠㅠ';
+        $og_url = G5_IMG_URL.'/no.png';
+    }
     include_once("../_head.php");
+    goto_url(G5_URL);
 ?>
 
 <?
